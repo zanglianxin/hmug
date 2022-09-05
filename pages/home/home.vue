3ec208befe8c975fbc329a7822b4c59fea081157
fengbujue <1125238900@qq.com>
2022-09-05 11:41:20 +0800

    完成布局

---
+++ pages/home/home.vue
@@ -0,0 +1,41 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+.topSearch {
+  width: 750rpx;
+  height: 70px;
+  background-color: #c00000;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  .search_btn {
+    width: 450rpx;
+    height: 30px;
+    margin-top: 25px;
+    margin-right: 50px;
+    background-color: #fff;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
