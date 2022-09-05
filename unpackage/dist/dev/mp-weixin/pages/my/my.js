2bf41f8c0852826bafacb6a7ea8eed3b06d0e122
fengbujue <1125238900@qq.com>
2022-09-05 11:42:35 +0800

    完成布局

---
+++ unpackage/dist/dev/mp-weixin/pages/my/my.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/my/my"],{
+
+/***/ 30:
+/*!******************************************************************************!*\
+  !*** D:/黑马程序员-前端培训/就业班/微信小程序原生/code/hmug/main.js?{"page":"pages%2Fmy%2Fmy"} ***!
+  \******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _my = _interopRequireDefault(__webpack_require__(/*! ./pages/my/my.vue */ 31));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_my.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 31:
+/*!***********************************************************!*\
+  !*** D:/黑马程序员-前端培训/就业班/微信小程序原生/code/hmug/pages/my/my.vue ***!
+  \***********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'D:\\黑马程序员-前端培训\\就业班\\微信小程序原生\\code\\hmug\\pages\\my\\my.vue'");
+
+/***/ })
+
+},[[30,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/my/my.js.map
